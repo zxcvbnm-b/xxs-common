f600c3a3a301fbee3893dd3bfa0a4f18292850f3
xiongsongxu_d <1605659095@qq.com>
2023-04-25 13:34:44 +0800

    代码重构

--- src/main/java/xxs/common/module/codegenerate/config/DataSourceConfig.java
+++ src/main/java/xxs/common/module/codegenerate/config/DataSourceConfig.java
@@ -8,8 +8,8 @@ import lombok.Data;
  */
 @Data
 public class DataSourceConfig {
-    private String jdbcUrl = "jdbc:mysql://10.19.1.82:3306/dolphinscheduler205?useUnicode=true&characterEncoding=UTF-8&useSSL=false&nullCatalogMeansCurrent=true";
-    private String jdbcUsername = "dolphinscheduler";
-    private String jdbcPassword = "Caih_123456";
+    private String jdbcUrl = "jdbc:mysql://127.0.0.1:3306/xxs?serverTimezone=PRC&nullCatalogMeansCurrent=true";
+    private String jdbcUsername = "root";
+    private String jdbcPassword = "051515";
     private String driverClassName = "com.mysql.cj.jdbc.Driver";
 }
